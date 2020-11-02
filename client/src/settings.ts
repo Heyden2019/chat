baa4d92aef0f22337979677602aa7cdbfadaf98c
Heyden2019 <master.kazya@yandex.ru>
2020-11-02 18:56:16 +0300

    heroku

--- client/src/settings.ts
+++ client/src/settings.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = process.env.NODE_ENV === "production" ? process.env.BASE_URL as string : `http://localhost:5000`
-export const API_URL = BASE_URL + "/api"
+export const BASE_URL = process.env.NODE_ENV === "production" ? "/" : `http://localhost:5000/`
+export const API_URL = BASE_URL + "api"
 export const USERS_PAGINATION_PORTION = 20
